6ff5817cf7ca1e18d48b2880ff8c9a1786bfb484
sealocal <local.mat@gmail.com>
2019-12-11 22:21:39 -0800

    add more dump formats

--- backup.sh
+++ backup.sh
@@ -66,19 +66,78 @@ printf "${Green}Dump backup of DATABASE_URL ...${EC}\n"
 
 # Borrow pg_dump flags from pg:dump per heroku support article
 # https://help.heroku.com/7U1BTYHB/how-can-i-take-a-logical-backup-of-large-heroku-postgres-databases
+
+# Backup with plain format
+printf "dump plain format ...\n"
 time pg_dump \
   --no-acl --no-owner --quote-all-identifiers \
   --format=plain --compress=4 \
   $DATABASE_URL > ./"${FILENAME}"_plain_format.sql.gz
 
-# Encrypt the plain format backup
+printf "Encrypt the plain format backup ...\n"
 openssl enc -aes-256-cbc -e -pass "env:DB_BACKUP_ENC_KEY" \
   -in ./"${FILENAME}"_plain_format.sql.gz \
   -out /tmp/"${FILENAME}"_plain_format.gz.enc
 
-printf "${Green}Copy Postgres dump to AWS S3 at S3_BUCKET_PATH...${EC}\n"
-time /app/vendor/awscli/bin/aws s3 cp /tmp/"${FILENAME}"_plain_format.gz.enc s3://$S3_BUCKET_PATH/$DBNAME/"${FILENAME}"_plain_format.gz.enc --expires $EXPIRATION_DATE
+printf "dump custom format ...\n"
+time pg_dump \
+  --no-acl --no-owner --quote-all-identifiers \
+  --format=custom \
+  $DATABASE_URL > ./"${FILENAME}"_custom_format.dump
 
-# Remove the database dump(s) from the app server
+printf "Encrypt the custom format backup"
+openssl enc -aes-256-cbc -e -pass "env:DB_BACKUP_ENC_KEY" \
+  -in ./"${FILENAME}"_custom_format.dump \
+  -out /tmp/"${FILENAME}"_custom_format.enc
+
+printf "dump directory format ...\n"
+time pg_dump \
+  --no-acl --no-owner --quote-all-identifiers \
+  --format=directory \
+  --file=./"${FILENAME}"_directory_format $DATABASE_URL
+
+printf "compress directory format ...\n"
+tar -zcvf ./"${FILENAME}"_directory_format.tar.gz ./"${FILENAME}"_directory_format/
+
+printf "Encrypt the directory format backup ...\n"
+openssl enc -aes-256-cbc -e -pass "env:DB_BACKUP_ENC_KEY" \
+  -in ./"${FILENAME}"_directory_format.tar.gz \
+  -out /tmp/"${FILENAME}"_directory_format.gz.enc
+
+printf "dump tar format ...\n"
+time pg_dump \
+  --no-acl --no-owner --quote-all-identifiers \
+  --format=tar \
+  --file=./"${FILENAME}"_tar_format.tar $DATABASE_URL
+
+printf "Encrypt the tar format backup ...\n"
+openssl enc -aes-256-cbc -e -pass "env:DB_BACKUP_ENC_KEY" \
+  -in ./"${FILENAME}"_tar_format.tar \
+  -out /tmp/"${FILENAME}"_tar_format.enc
+
+printf "${Green}Copy Postgres dumps to AWS S3 at S3_BUCKET_PATH...${EC}\n"
+printf "upload plain format ...\n"
+time /app/vendor/awscli/bin/aws s3 cp \
+  /tmp/"${FILENAME}"_plain_format.gz.enc \
+  s3://$S3_BUCKET_PATH/$DBNAME/"${FILENAME}"_plain_format.gz.enc --expires $EXPIRATION_DATE
+
+printf "upload custom format ...\n"
+time /app/vendor/awscli/bin/aws s3 cp \
+  /tmp/"${FILENAME}"_custom_format.enc \
+  s3://$S3_BUCKET_PATH/$DBNAME/"${FILENAME}"_custom_format.enc --expires $EXPIRATION_DATE
+
+printf "upload directroy format ...\n"
+time /app/vendor/awscli/bin/aws s3 cp \
+  /tmp/"${FILENAME}"_directory_format.gz.enc \
+  s3://$S3_BUCKET_PATH/$DBNAME/"${FILENAME}"_directory_format.gz.enc --expires $EXPIRATION_DATE
+
+printf "upload tar format ...\n"
+time /app/vendor/awscli/bin/aws s3 cp \
+  /tmp/"${FILENAME}"_tar_format.enc \
+  s3://$S3_BUCKET_PATH/$DBNAME/"${FILENAME}"_tar_format.enc --expires $EXPIRATION_DATE
+
+# Remove the database dumps from the app server
 rm -v /tmp/"${FILENAME}"_plain_format.gz.enc ./"${FILENAME}"_plain_format.sql.gz
-
+rm -v /tmp/"${FILENAME}"_custom_format.enc ./"${FILENAME}"_custom_format.dump
+rm -rv /tmp/"${FILENAME}"_directory_format.gz.enc ./"${FILENAME}"_directory_format.tar.gz ./"${FILENAME}"_directory_format
+rm -v /tmp/"${FILENAME}"_tar_format.enc ./"${FILENAME}"_tar_format.tar
